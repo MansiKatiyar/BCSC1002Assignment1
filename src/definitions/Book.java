713fb1e1110bc844930b695a224fde5348d16035
Mansi katiyar <mansi.katiyar_cs19@gla.ac.in>
2020-08-28 19:57:06 +0530

    create the getter and setter method for the private field bookName.

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -1,14 +1,22 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 21/08/20
- *  Time: 3:49 PM
+ *  User: Mansi katiyar
+ *  Date: 28/08/20
+ *  Time: 7:30 PM
  *  File Name : Book.java
  * */
 package definitions;
 
 public class Book {
     private  String  bookName;
-    private String authorName;
+    private String bookAuthorName;
     private String ISBN;
+
+    public String getBookName(){
+        return null;
+    }
+
+    public void setBookName(String bookName){
+      this.bookName = bookName;
+    }
 
 }
